4349d7a7fdd404af1503b2aabcfdc806fc2d9e40
Kevin Cittadini <kevincittadini@MacBook-Pro-di-Kevin.local>
2021-10-12 14:39:50 +0200

    Restore install script.

---
+++ .docker/install_ps.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mysql -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD -e "drop database if exists $DB_NAME;"
+mysqladmin -h $DB_SERVER -P $DB_PORT -u $DB_USER -p$DB_PASSWD create $DB_NAME --force;
+
+export PS_DOMAIN=$(hostname -i)
+
+echo "\n* Launching the installer script..."
+runuser -g www-data -u www-data -- php /var/www/html/$PS_FOLDER_INSTALL/index_cli.php \
+    --domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
+    --db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
+    --password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
+    --all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL
